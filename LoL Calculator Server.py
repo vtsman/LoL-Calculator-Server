435cdd66f4dba88266b6da361c2e4c115a75bb38
vtsman <hokiespencer@gmail.com>
2015-08-10 17:10:46 -0400

    flush cache every day

--- LoL Calculator Server.py
+++ LoL Calculator Server.py
@@ -4,6 +4,8 @@ import os
 import json
 import requests
 from crossdomain import crossdomain
+import time
+import thread
 
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -14,6 +16,7 @@ max_calls = 100
 
 app = Flask(__name__)
 
+
 def call_api(url, args):
     global api_key
     req_url = api_url + url
@@ -21,11 +24,14 @@ def call_api(url, args):
     send_args["api_key"] = api_key
     return requests.get(req_url, params=send_args)
 
+
 def get_json(response):
     return json.dumps(response.json())
 
+
 #hacked together request limiter
 #change at some point
+
 
 @app.route('/' , defaults={'path': ''})
 @app.route('/<path:path>')
@@ -48,6 +54,17 @@ def catch_all(path):
         return get_json(call_api(path, request.args))
     return "Error: Too many api calls", 401
 
+
+def reset_cache(tName):
+    global cached_calls
+    global ip_req_count
+    while True:
+        time.sleep(86400)
+        cached_calls = []
+        ip_req_count = []
+        print("flushed cache")
+
+
 if __name__ == '__main__':
     global api_key
     global cached_calls
@@ -56,4 +73,7 @@ if __name__ == '__main__':
     ip_req_count = dict()
     api_key = open(os.path.join(__location__, "api.key")).read()
     print(api_key)
-    app.run()
+
+    thread.start_new_thread(reset_cache, ("Cache reset", ))
+
+    app.run(host='0.0.0.0', port=1081)
